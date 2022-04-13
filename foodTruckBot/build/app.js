8a7472bdb61b15668353cd3b69ff081e14cfe49a
Amy Gebhardt <amy.gebhardt@code.org>
2022-04-13 17:12:21 -0500

    created an initial typescript function to experiment with

---
+++ foodTruckBot/build/app.js
@@ -0,0 +1,16 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.lambdaHandler = void 0;
+const seattleFoodTruckUrl = "https://www.seattlefoodtruck.com/api/locations?include_events=true&include_trucks=true&only_with_events=true&with_active_trucks=true&neighborhood=downtown&with_events_on_day=";
+const lambdaHandler = async (event) => {
+    const colors = ["#00adbc", "7665a0"];
+    // ko-KR allows the year-month-day
+    const today = new Date().toLocaleDateString('ko-KR', { year: "numeric", month: "2-digit", day: "2-digit" }).replace(/\. /g, '-').replace(/\./g, '');
+    const url = `${seattleFoodTruckUrl}${today}`;
+    console.log('Hooray! A URL! In a Lamda!');
+    console.log(url);
+    return;
+};
+exports.lambdaHandler = lambdaHandler;
+(0, exports.lambdaHandler)({});
+//# sourceMappingURL=app.js.map
